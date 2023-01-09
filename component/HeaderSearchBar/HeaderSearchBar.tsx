f30f470cd3b25a61e4520582e8e981cb02573560
ndkn <nguyentuanpkn@gmail.com>
2023-01-09 14:56:20 +0700

    SafeAreaView on HeaderSearchBar

--- component/HeaderSearchBar/HeaderSearchBar.tsx
+++ component/HeaderSearchBar/HeaderSearchBar.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainerProps } from '@react-navigation/native';
 import { Input } from '@rneui/base';
 import React, { PropsWithChildren, useEffect, useRef, useState } from 'react'
-import { TouchableOpacity, View, Image, TextInput, Animated, Easing, NativeSyntheticEvent, TextInputFocusEventData } from 'react-native'
+import { TouchableOpacity, View, Image, TextInput, Animated, Easing, NativeSyntheticEvent, TextInputFocusEventData, SafeAreaView } from 'react-native'
 import arrowBack from "../../assets/icons/arrow-back-white.png";
 import searchWhite from "../../assets/icons/search-white.png";
 import { paletteGray, primaryColor } from '../../utils/color';
@@ -14,33 +14,35 @@ interface HeaderSearchBarProps extends PropsWithChildren {
 }
 function HeaderSearchBar(props: HeaderSearchBarProps) {
     return (
-        <View style={{ height: "100%" }}>
-            <View
-                style={{
-                    //borderWidth: 1,
-                    flexDirection: "row",
-                    width: "85%"
-                }}>
-                <Input
-                    onSubmitEditing={props.onSubmit}
-                    value={props.value}
-                    onChangeText={props.onChangeText}
-                    placeholderTextColor={paletteGray}
-                    placeholder="Tìm kiếm"
+        <SafeAreaView>
+            <View style={{ height: "100%" }}>
+                <View
                     style={{
-                        color : "white",
-                        fontSize: 16,
-                        borderBottomWidth: 1,
-                        borderBottomColor: "white",
-                        width: "100%"
-                    }} />
-                <View style={{ width: "15%" }}>
-                    <TouchableOpacity onPress={props.onSubmit} style={{ alignItems: "center", justifyContent: "center", width: 30, height: "100%" }}>
-                        <Image source={searchWhite} style={{ width: "100%" }} resizeMode="contain" />
-                    </TouchableOpacity>
+                        //borderWidth: 1,
+                        flexDirection: "row",
+                        width: "85%"
+                    }}>
+                    <Input
+                        onSubmitEditing={props.onSubmit}
+                        value={props.value}
+                        onChangeText={props.onChangeText}
+                        placeholderTextColor={paletteGray}
+                        placeholder="Tìm kiếm"
+                        style={{
+                            color: "white",
+                            fontSize: 16,
+                            borderBottomWidth: 1,
+                            borderBottomColor: "white",
+                            width: "100%"
+                        }} />
+                    <View style={{ width: "15%" }}>
+                        <TouchableOpacity onPress={props.onSubmit} style={{ alignItems: "center", justifyContent: "center", width: 30, height: "100%" }}>
+                            <Image source={searchWhite} style={{ width: "100%" }} resizeMode="contain" />
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-        </View>
+        </SafeAreaView>
     )
 }
 
